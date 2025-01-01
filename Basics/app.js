377f6ad0668f1efd3d1a1820152ebddc7a1aee3d
shahzad2121 <shahzadnadeem.dev@gmail.com>
2025-01-02 02:30:41 +0500

    CRUD Operations: Create learned

--- Basics/app.js
+++ Basics/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const userModel = require('./models/user')
-const mongoserver = require('./config/db')
+const userModel = require("./models/user");
+const mongoserver = require("./config/db");
 
 //this is  view engine usage method
 app.set("view engine", "ejs");
@@ -22,12 +22,27 @@ app.set("view engine", "ejs");
 // });
 
 //built-in middlewares
-app.use(express())
-app.use(express.urlencoded({extended:true}))
-app.use(express.static('public'))
+app.use(express());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  const newUser = await userModel.create({
+    username: username,
+    email: email,
+    password: password,
+  });
+  //console.log(req.body);
+  res.send("registered");
 });
 app.get("/about", (req, res) => {
   res.send("Software engineer");
@@ -38,8 +53,7 @@ app.get("/project", (req, res) => {
 app.post("/form-data", (req, res) => {
   console.log(req.body);
 
-  res.send('logged in')
-  
+  res.send("logged in");
 });
 
 app.listen(3000);
