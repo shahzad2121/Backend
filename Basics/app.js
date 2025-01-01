d938814bae864cba97b88e3a8c1134eba84da951
shahzad2121 <shahzadnadeem.dev@gmail.com>
2025-01-02 01:34:16 +0500

    progress

---
+++ Basics/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const userModel = require('./models/user')
+const mongoserver = require('./config/db')
+
+//this is  view engine usage method
+app.set("view engine", "ejs");
+
+//Middle-Wares
+//there are three types of middlwares:
+// 1:custom;
+// 2:express prebuilt
+// 3:third-party
+
+//this is custom middle-ware
+// app.use((req, res, next) => {
+//   console.log("this is middware");
+//   var a = 10.17;
+//   var b = 20.6;
+//   console.log(a + b);
+//   return next();
+// });
+
+//built-in middlewares
+app.use(express())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.send("Software engineer");
+});
+app.get("/project", (req, res) => {
+  res.send("working on them");
+});
+app.post("/form-data", (req, res) => {
+  console.log(req.body);
+
+  res.send('logged in')
+  
+});
+
+app.listen(3000);
---
+++ Basics/config/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const mongoserver = mongoose.connect('mongodb://0.0.0.0/basics').then(() => {
+    console.log('connected to DB');
+    
+})
+
+module.exports = mongoserver
